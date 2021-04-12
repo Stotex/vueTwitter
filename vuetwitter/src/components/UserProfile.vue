2bfc09ed1400bdfb7b2af47cb28e727079139d89
Kenno Pajunurm <33248162+Stotex@users.noreply.github.com>
2021-04-13 02:41:45 +0300

    Commit

---
+++ vuetwitter/src/components/UserProfile.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__follower-count">
+        <strong>Followers:</strong> {{ followers }}
+      </div>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+    </div>
+    <div class="user-profile__tweets-wrapper">
+      <TweetItem
+        v-for="tweet in user.tweets"
+        :key="tweet.id"
+        :username="user.username"
+        :tweet="tweet"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import TweetItem from "./TweetItem";
+
+export default {
+  name: "UserProfile",
+  components: { TweetItem },
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "Kenno905",
+        firstName: "Kenno",
+        lastName: "Pajunurm",
+        email: "kennopajunurm@hotmail.com",
+        isAdmin: true,
+        tweets: [
+          { id: 1, content: "Vue seems interesting!" },
+          {
+            id: 2,
+            content: "I am looking for cool projects to develop my skills!",
+          },
+        ],
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`);
+      }
+    },
+  },
+  computed: {
+    fullname() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+}
+
+.user-profile__admin-badge {
+  border: 1px solid black;
+  margin-right: auto;
+  font-weight: bold;
+  padding: 2px;
+}
+
+h1 {
+  margin: 0;
+}
+</style>
